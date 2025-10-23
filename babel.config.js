2bef36c8d5ce7c46fc94f4650d46479c685ff49b
Karthick Elumalai <kelumalai@presidio.com>
2025-10-23 18:54:41 +0530

    WIP: saving local changes before pulling

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push("react-native-worklets/plugin");
-
+  
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel"
     ],
-    plugins
+    plugins: [
+      // react-native-reanimated/plugin must be listed last
+      "react-native-reanimated/plugin"
+    ]
   };
 };
